2efa93a79301e14405a644fc1e3cfda271f3bdf1
Vinicius Azevedo <vinicius.azevedo@madeiramadeira.com.br>
2025-07-25 19:45:55 -0300

    implement main termx API with all component exports

---
+++ termx.go
@@ -0,0 +1,60 @@
+package termx
+
+import (
+	"github.com/vynazevedo/termx/ascii"
+	"github.com/vynazevedo/termx/chart"
+	"github.com/vynazevedo/termx/confirm"
+	"github.com/vynazevedo/termx/form"
+	"github.com/vynazevedo/termx/input"
+	"github.com/vynazevedo/termx/layout"
+	"github.com/vynazevedo/termx/progress"
+	"github.com/vynazevedo/termx/renderer"
+	"github.com/vynazevedo/termx/selector"
+	"github.com/vynazevedo/termx/table"
+	"github.com/vynazevedo/termx/theme"
+)
+
+var (
+	Form    = form.New
+	Input   = input.New
+	Select  = selector.New
+	Confirm = confirm.New
+	Table   = table.New
+	Chart   = chart.New
+	
+	ASCII = ascii.New
+	Box   = ascii.Box
+	
+	Progress = progress.NewBar
+	Spinner  = progress.NewSpinner
+	
+	Split    = layout.NewSplit
+	BoxLayout = layout.NewBox
+	
+	Required  = input.Required
+	MinLength = input.MinLength
+	MaxLength = input.MaxLength
+	Email     = input.Email
+	
+	WithPlaceholder = form.WithPlaceholder
+	WithValidator   = form.WithValidator
+	WithMaxLength   = form.WithMaxLength
+)
+
+const (
+	KubernetesLogo = ascii.KubernetesLogo
+	DockerLogo     = ascii.DockerLogo
+	ServerRack     = ascii.ServerRack
+)
+
+func SetTheme(t *theme.Theme) {
+	theme.Set(t)
+}
+
+func GetTheme() *theme.Theme {
+	return theme.Current()
+}
+
+func ClearScreen() {
+	renderer.ClearScreen()
+}
